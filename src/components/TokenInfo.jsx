5f95accf6ab46580b03c75798fbcab99aebd33ce
Vitor Butkus <vitorbutkus@gmail.com>
2024-03-19 19:01:13 -0300

    remove overlay in TokenInfo

--- src/components/TokenInfo.jsx
+++ src/components/TokenInfo.jsx
@@ -83,111 +83,106 @@ export default function TokenInfo({ openTokenData, handleClickOverlay }) {
   return (
     <>
       <motion.div
-        className='absolute top-0 left-0 w-screen h-screen flex items-start justify-end z-[2]'
-        onClick={handleClickOverlay}
+        className='absolute top-0 right-0 max-w-[500px] bg-slate-700 pb-6 z-[1] h-[calc(100%)]'
+        onClick={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+        }}
         variants={boxVariants}
         initial='hidden'
         animate='visible'
         exit='exit'
       >
+        {/* close button */}
         <div
-          className='bg-slate-700 pb-6 z-[1] h-[calc(100%-100px)]'
-          onClick={(e) => {
-            e.preventDefault();
-            e.stopPropagation();
-          }}
+          className='w-full flex justify-center p-1 mb-4 bg-slate-500 hover:bg-slate-400 active:bg-slate-400 duration-300 cursor-pointer'
+          onClick={handleClickOverlay}
         >
-          {/* close button */}
-          <div
-            className='w-full flex justify-center p-1 mb-4 bg-slate-500 hover:bg-slate-400 active:bg-slate-400 duration-300 cursor-pointer'
-            onClick={handleClickOverlay}
-          >
-            <RxChevronDown size={24} />
-          </div>
-          {/* columns */}
-          <div className='pl-4 flex justify-between gap-4 max-w-[500px]'>
-            <div className='flex flex-col gap-4'>
-              <div className='relative'>
-                <img
-                  src={openTokenData.token.image}
-                  alt={openTokenData.token.name}
-                  width={250}
-                  height={250}
-                  className='max-w-[250px] max-h-[250px] object-contain rounded-md bg-white/10'
-                />
-                <div
-                  className='absolute bottom-2 right-2 cursor-pointer'
-                  onClick={() => setOpenLargeMedia(openTokenData)}
-                >
-                  <BsArrowsFullscreen size='12' />
-                </div>
-              </div>
-              <div className=''>
-                <CollectModal
-                  type='mint'
-                  trigger={
-                    <button
-                      className='w-full bg-slate-800/90 text-white/90 py-2 rounded-md hover:bg-[#01ff00] hover:text-black hover:scale-105 transition-all duration-300'
-                      //   onClick={() => setMintOpenState(true)}
-                    >
-                      Mint
-                    </button>
-                  }
-                  onConnectWallet={() => {
-                    openConnectModal?.();
-                  }}
-                  defaultQuantity={2}
-                  // openState={mintOpenState}
-                  collectionId={openTokenData.token.contract}
-                  tokenId={openTokenData.token.tokenId}
-                  onCollectComplete={(data) => console.log(data)}
-                  onCollectError={(error) => console.log(error)}
-
-                  // token={
-                  //   openTokenData.token.contract +
-                  //   ':' +
-                  //   openTokenData.token.tokenId
-                  // }
-                />
-              </div>
-              <div className='grid grid-cols-2 gap-2'>
-                {openTokenData.token.attributes.map((att) => (
-                  <div
-                    key={att.key}
-                    className='flex flex-col gap-2 text-xs bg-white/10 rounded-md pb-2 h-fit'
-                  >
-                    <div className='text-center text-[10px] bg-white/10'>
-                      {att.key}
-                    </div>
-                    <div className='text-center px-2 flex flex-col gap-1'>
-                      {att.key !== 'Content Tags'
-                        ? att.value
-                        : att.value
-                            .split(',')
-                            .map((tag) => <div key={tag}>{tag.trim()}</div>)}
-                    </div>
-                  </div>
-                ))}
+          <RxChevronDown size={24} />
+        </div>
+        {/* columns */}
+        <div className='pl-4 flex justify-between gap-4 max-w-[500px]'>
+          <div className='flex flex-col gap-4'>
+            <div className='relative'>
+              <img
+                src={openTokenData.token.image}
+                alt={openTokenData.token.name}
+                width={250}
+                height={250}
+                className='max-w-[250px] max-h-[250px] object-contain rounded-md bg-white/10'
+              />
+              <div
+                className='absolute bottom-2 right-2 cursor-pointer'
+                onClick={() => setOpenLargeMedia(openTokenData)}
+              >
+                <BsArrowsFullscreen size='12' />
               </div>
             </div>
+            <div className=''>
+              <CollectModal
+                type='mint'
+                trigger={
+                  <button
+                    className='w-full bg-slate-800/90 text-white/90 py-2 rounded-md hover:bg-[#01ff00] hover:text-black hover:scale-105 transition-all duration-300'
+                    //   onClick={() => setMintOpenState(true)}
+                  >
+                    Mint
+                  </button>
+                }
+                onConnectWallet={() => {
+                  openConnectModal?.();
+                }}
+                defaultQuantity={2}
+                // openState={mintOpenState}
+                collectionId={openTokenData.token.contract}
+                tokenId={openTokenData.token.tokenId}
+                onCollectComplete={(data) => console.log(data)}
+                onCollectError={(error) => console.log(error)}
 
-            <div className='flex flex-col gap-1'>
-              <div className='pr-4'>{openTokenData.token.name}</div>
-              <div className='text-sm pr-4'>
-                Created by{' '}
-                <span className='font-bold'>
-                  {
-                    openTokenData.token.attributes.find(
-                      (att) => att.key === 'Creator'
-                    ).value
-                  }
-                </span>
-              </div>
-              <div className='mt-4 pr-3 text-sm max-h-[calc(100vh-222px)] overflow-scroll'>
-                <Markdown className=''>
-                  {openTokenData.token.description}
-                </Markdown>
-              </div>
+                // token={
+                //   openTokenData.token.contract +
+                //   ':' +
+                //   openTokenData.token.tokenId
+                // }
+              />
+            </div>
+            <div className='grid grid-cols-2 gap-2'>
+              {openTokenData.token.attributes.map((att) => (
+                <div
+                  key={att.key}
+                  className='flex flex-col gap-2 text-xs bg-white/10 rounded-md pb-2 h-fit'
+                >
+                  <div className='text-center text-[10px] bg-white/10'>
+                    {att.key}
+                  </div>
+                  <div className='text-center px-2 flex flex-col gap-1'>
+                    {att.key !== 'Content Tags'
+                      ? att.value
+                      : att.value
+                          .split(',')
+                          .map((tag) => <div key={tag}>{tag.trim()}</div>)}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <div className='pr-4'>{openTokenData.token.name}</div>
+            <div className='text-sm pr-4'>
+              Created by{' '}
+              <span className='font-bold'>
+                {
+                  openTokenData.token.attributes.find(
+                    (att) => att.key === 'Creator'
+                  ).value
+                }
+              </span>
+            </div>
+            <div className='mt-4 pr-3 text-sm max-h-[calc(100vh-222px)] overflow-scroll'>
+              <Markdown className=''>
+                {openTokenData.token.description}
+              </Markdown>
             </div>
           </div>
         </div>
