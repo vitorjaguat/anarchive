63bc67b4c3c62f18663c128c17b1d0fdf56083a2
Vitor Butkus <vitorbutkus@gmail.com>
2024-03-24 12:09:46 -0300

    now loading pdfs in LargeMedia

--- src/components/LargeMedia.jsx
+++ src/components/LargeMedia.jsx
@@ -1,11 +1,73 @@
 import { TokenMedia } from '@reservoir0x/reservoir-kit-ui';
+import { useEffect, useState } from 'react';
 
 export default function LargeMedia({ token }) {
+  //   const [file, setFile] = useState(null);
+
+  //   useEffect(() => {
+  //     const fetchFile = async () => {
+  //       try {
+  //         const response = await fetch(
+  //           'https://img.reservoir.tools/images/v2/zora/i9YO%2F4yHXUdJsWcTqhqvf%2FVBd%2FGKEcuTw3LiAUIsDnndtIVQjh5bveWRv9rFS91v%2FrQ7NWrfitYfPBBKlX5bDDS42PGupsw8hYYHduF1CeCoJ2ciACcORBcP67fMFZU2.gltf-binary'
+  //         );
+
+  //         if (!response.ok) {
+  //           throw new Error('Failed to fetch file');
+  //         }
+
+  //         const blob = await response.blob();
+  //         const newMedia = URL.createObjectURL(blob);
+  //         setFile(newMedia);
+  //       } catch (error) {
+  //         console.error('Error fetching file:', error);
+  //       }
+  //     };
+  //     if (token.media?.includes('.gltf')) {
+  //       fetchFile();
+  //     }
+  //   }, [token]);
+
+  //   if (token.media?.includes('.gltf')) {
+  //     console.log('token.media', token.media);
+  //     return (
+  //       <model-viewer
+  //         src={token.media}
+  //         alt={token.name}
+  //         auto-rotate
+  //         camera-controls
+  //       ></model-viewer>
+  //     );
+  //   }
+
   if (token.media?.includes('.mpeg')) {
     return (
       <audio controls autoPlay>
         <source src={token.media} type='audio/mpeg' />
       </audio>
+    );
+  }
+
+  if (token.media?.includes('.pdf')) {
+    return (
+      //   <embed
+      //     src={token.media}
+      //     type='application/pdf'
+      //     width='500px'
+      //     height='1000px'
+      //   />
+
+      //   <iframe src={token.media} width='100%' height='100%' ></iframe>
+
+      <object
+        // style={{
+        //   width: '100%',
+        //   height: '100%',
+        //   aspectRatio: '4/3',
+        // }}
+        data={token.media}
+        width='800'
+        height='500'
+      ></object>
     );
   }
 
---
+++ src/components/LargePDF.jsx
@@ -0,0 +1,3 @@
+export default function LargePDF() {
+  return <div>LargePDF</div>;
+}
