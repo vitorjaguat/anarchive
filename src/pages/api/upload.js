d916d9f580a54182eb7e8f895012eca2a586ecdd
Vitor Butkus <vitorbutkus@gmail.com>
2024-06-25 19:32:56 -0300

    adjust html, body, main in globals

--- src/pages/api/upload.js
+++ src/pages/api/upload.js
@@ -12,9 +12,6 @@ export const config = {
 const storage = new ThirdwebStorage({
   clientId: process.env.THIRDWEB_CLIENT_ID,
   secretKey: process.env.THIRDWEB_CLIENT_SECRET,
-  // clientId: 'd3cb6e160e5c6817e0ba2c74376ddd7b',
-  // secretKey:
-  //   'CKVMMqO6iJ6XoEpXTvDjdZMwYBaCmoFOGL2rCBV8K_l9p1XlPOgSWjC5m1_qDq7GhDjNS58eWheo-UQENZp0ig',
 });
 
 export default async function handler(req, res) {
@@ -25,7 +22,6 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
       const form = new IncomingForm();
-      let uri;
       const [fields, files] = await form.parse(req);
       console.log('fields:', fields);
       console.log('files:', files);
@@ -34,7 +30,9 @@ export default async function handler(req, res) {
       // const imgPath = path.join(process.cwd(), files.image[0].filepath);
       // console.log('imgPath:', imgPath);
       const img = fs.readFileSync(files.image[0].filepath);
-      uri = await storage.upload(img);
+      const media = fs.readFileSync(files.media[0].filepath);
+      const uriImg = await storage.upload(img);
+      const uriMedia = await storage.upload(media);
 
       // form.parse(req, async (err, fields, files) => {
       //   console.log('fields:', fields);
@@ -58,9 +56,12 @@ export default async function handler(req, res) {
       // console.log('download URL:', result.body);
 
       // res.status(200).json({ message: 'Image uploaded successfully', uri });
-      console.log('upload URL:', uri);
+      console.log('image URI:', uriImg);
+      console.log('media URI:', uriMedia);
 
-      res.status(200).json({ message: 'Image uploaded successfully', uri });
+      res
+        .status(200)
+        .json({ message: 'Image uploaded successfully', uriImg, uriMedia });
     } catch (error) {
       console.error('Error:', error);
       res.status(500).json({ error: 'Error uploading image' });
