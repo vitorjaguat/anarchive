dde0b880d2a7da60a48061f91a448c53fa6a0734
Vitor Butkus <vitorbutkus@gmail.com>
2024-07-08 19:47:20 -0300

    set description and title to be dynamic in Headhead, besides ogImage

--- src/components/Headhead.jsx
+++ src/components/Headhead.jsx
@@ -2,7 +2,14 @@ import Head from 'next/head';
 // import favicon from '../../public/meta/image1.png';
 // import ogimage from '../../public/meta/image2.png';
 
-export default function Headhead({ ogImage }) {
+export default function Headhead({ ogImage, tokenDataForOG }) {
+  const ogImageURL = ogImage;
+  const title = tokenDataForOG?.token
+    ? tokenDataForOG?.token?.name + ' | The Anarchiving Game'
+    : 'The Anarchiving Game';
+  const description = tokenDataForOG?.token
+    ? tokenDataForOG?.token?.description?.slice(0, 130) + '...'
+    : "A dynamic, participatory open canvas where community's memories and creativity are continuously interpreted and reimagined.";
   return (
     <>
       <Head>
@@ -15,15 +22,14 @@ export default function Headhead({ ogImage }) {
         <title>The Anarchiving Game</title>
         <meta
           name='description'
-          content="A dynamic, participatory open canvas where community's memories and creativity are continuously interpreted and reimagined."
+          content={
+            "A dynamic, participatory open canvas where community's memories and creativity are continuously interpreted and reimagined."
+          }
         />
         <meta name='twitter:card' content='summary_large_image' />
         <meta name='twitter:site' content='@thesphere_as' />
-        <meta name='twitter:title' content='The Anarchiving Game' />
-        <meta
-          name='twitter:description'
-          content="A dynamic, participatory open canvas where community's memories and creativity are continuously interpreted and reimagined,"
-        />
+        <meta name='twitter:title' content={title} />
+        <meta name='twitter:description' content={description} />
         <meta
           name='twitter:image'
           // content='https://the-anarchive.vercel.app/meta/image2.png'
@@ -32,11 +38,8 @@ export default function Headhead({ ogImage }) {
         <meta property='og:type' content='website' />
         <meta property='og:determiner' content='the' />
         <meta property='og:locale' content='en' />
-        <meta property='og:title' content='The Anarchiving Game' />
-        <meta
-          property='og:description'
-          content="A dynamic, participatory open canvas where community's memories and creativity are continuously interpreted and reimagined."
-        />
+        <meta property='og:title' content={title} />
+        <meta property='og:description' content={description} />
         <meta
           property='og:image'
           // content='https://the-anarchive.vercel.app/meta/image2.png'
@@ -45,7 +48,7 @@ export default function Headhead({ ogImage }) {
         <meta property='og:image:type' content='image/png' />
         <meta property='og:image:width' content='1200' />
         <meta property='og:image:height' content='630' />
-        <meta property='og:image:alt' content='The Anarchiving Game' />
+        <meta property='og:image:alt' content={title} />
       </Head>
     </>
   );
