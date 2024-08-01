045221f25dffe786312634ef8c04994c1f8565a1
Vitor Butkus <vitorbutkus@gmail.com>
2024-08-01 12:35:49 -0300

    return with CreateTokenButton in index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -129,7 +129,7 @@ export default function Home({ allTokens, tokenDataForOG }) {
             setImageLoaded={setImageLoaded}
           />
 
-          {/* <CreateTokenButton /> */}
+          <CreateTokenButton />
         </main>
       </Layout>
     </>
