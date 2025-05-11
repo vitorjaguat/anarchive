eb8722e8ed4b9fa495a81acdd99566f31c83f01b
Vitor Butkus <vitorbutkus@gmail.com>
2025-05-11 15:32:23 -0300

    GridOpenToken with collect button ok

---
+++ src/components/mobile/GridOpenToken.tsx
@@ -0,0 +1,123 @@
+import Image from 'next/image';
+import type { ReservoirToken } from '../../../types/tokens';
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import { IoCloseOutline } from 'react-icons/io5';
+import { GoPlusCircle } from 'react-icons/go';
+import { MintModal } from '@reservoir0x/reservoir-kit-ui';
+
+export default function GridOpenToken({
+  token,
+  onClose,
+}: {
+  token: ReservoirToken;
+  onClose: () => void;
+}) {
+  useEffect(() => {
+    // Lock scroll
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, []);
+
+  return createPortal(
+    <div className='fixed inset-0 z-50 flex items-center justify-center bg-black/70'>
+      <div
+        className='bg-slate-500 rounded-lg max-w-lg w-full max-h-[90vh] overflow-y-auto p-4'
+        tabIndex={-1}
+      >
+        <button className='absolute top-2 right-2' onClick={onClose}>
+          <IoCloseOutline className='text-white' size={25} />
+        </button>
+        {/* IMAGE */}
+        <div className='w-full aspect-square flex items-center bg-black/20'>
+          {token?.token?.image && (
+            <Image
+              src={token.token.image}
+              alt={token.token.name}
+              layout='responsive'
+              width={1}
+              height={1}
+              className={'w-full cursor-pointer'}
+              //   onLoad={(e) => setImageLoaded(true)}
+              onClick={() => {
+                // largeMediaControls.start('visible');
+                // setOpenLargeMedia(token);
+              }}
+            />
+          )}
+        </div>
+        {/* COLLECT (LIKES) */}
+        <div className='mt-1 flex gap-2 items-center justify-between px-1'>
+          <div className='flex gap-2'>
+            <GoPlusCircle size={21} color='white' />
+            <div className=''>{token.token.supply}</div>
+          </div>
+          <MintModal
+            chainId={7777777}
+            copyOverrides={{
+              mintTitle: 'Collect your own',
+              mintCtaBuy: 'Collect',
+            }}
+            // normalizeRoyalties={true}
+            trigger={
+              <button className='px-4 py-1 rounded-md bg-[#01ff00] text-[#000000] hover:scale-[1.02] transition-all duration-300'>
+                Collect
+              </button>
+            }
+            // onConnectWallet={() => {
+            //   openConnectModal?.();
+            // }}
+            // referrerAddress='0xBFd118f0ff5d6f4D3Eb999eAF197Dbfcc421C5Ea'
+            // referrer='0xBFd118f0ff5d6f4D3Eb999eAF197Dbfcc421C5Ea'
+
+            // openState={mintOpenState}
+            // collectionId={openTokenData.token.contract}
+            // tokenId={openTokenData.token.tokenId}
+            onMintComplete={(data) => console.log(data)}
+            onMintError={(error) => console.log(error)}
+            onConnectWallet={() => {}}
+            token={token.token.contract + ':' + token.token.tokenId}
+          />
+          {/* <div className=''>Collect</div> */}
+        </div>
+
+        {/* DETAILS */}
+        <div className='flex flex-col gap-3 my-3 px-1'>
+          {/* TITLE */}
+          <div className=''>{token.token.name}</div>
+
+          {/* ATTRIBUTES */}
+          <div className='grid grid-cols-2 gap-1'>
+            {token?.token?.attributes?.map((att, i) => (
+              <div
+                key={i}
+                className='flex flex-col gap-1 text-xs bg-white/10 rounded-md pb-1 h-full'
+              >
+                <div className='text-left px-2 text-[10px] bg-white/10'>
+                  {att?.key}
+                </div>
+                <div className='text-left px-2 flex flex-col justify-center h-full gap-1 font-thin'>
+                  {att?.key !== 'Content Tags'
+                    ? att?.value
+                    : att?.value
+                        .split(',')
+                        .map((tag) => <div key={tag}>{tag.trim()}</div>)}
+                </div>
+              </div>
+            ))}
+          </div>
+
+          {/* DETAILS */}
+          <div className='w-full flex flex-col gap-1 px-1'>
+            <div className='text-sm text-gray-200 hyphens-auto'>
+              {token.token.description}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>,
+    document.body
+  );
+}
--- src/components/mobile/GridViewItemMobile.tsx
+++ src/components/mobile/GridViewItemMobile.tsx
@@ -4,46 +4,57 @@ import type {
   ReservoirTokensResponse,
   ReservoirToken,
 } from '../../../types/tokens';
-
-interface tokenType {
-  token: {
-    description: string;
-  };
-}
+import { useState } from 'react';
+import GridOpenToken from './GridOpenToken';
 
 export default function GridViewItemMobile({
   token,
 }: {
   token: ReservoirToken;
 }) {
+  const [openToken, setOpenToken] = useState<ReservoirToken | null>(null);
+
+  const handleClick = () => {
+    console.log('token', token);
+    setOpenToken(token);
+  };
   return (
-    <div className='flex flex-col items-center border-[1px] bg-purple-300/20 border-purple-300/20 rounded-lg w-full'>
-      {/* IMAGE */}
-      <div className='w-full aspect-square flex items-center bg-black/20'>
-        {token?.token?.image && (
-          <Image
-            src={token.token.image}
-            alt={token.token.name}
-            layout='responsive'
-            objectFit='contain'
-            width={1}
-            height={1}
-            className={'w-full cursor-pointer'}
-            //   onLoad={(e) => setImageLoaded(true)}
-            onClick={() => {
-              // largeMediaControls.start('visible');
-              // setOpenLargeMedia(token);
-            }}
-          />
-        )}
-      </div>
-      {/* DETAILS */}
-      <div className='w-full flex flex-col gap-1 py-2 px-1'>
-        <div className='my-1'>{token.token.name}</div>
-        <div className='text-sm text-gray-400 hyphens-auto'>
-          {token.token.description}
+    <>
+      <div
+        className='flex flex-col items-center border-[1px] bg-purple-300/20 border-purple-300/20 rounded-lg w-full'
+        onClick={handleClick}
+      >
+        {/* IMAGE */}
+        <div className='w-full aspect-square flex items-center bg-black/20'>
+          {token?.token?.image && (
+            <Image
+              src={token.token.image}
+              alt={token.token.name}
+              layout='responsive'
+              width={1}
+              height={1}
+              className={'w-full cursor-pointer'}
+              //   onLoad={(e) => setImageLoaded(true)}
+              onClick={() => {
+                // largeMediaControls.start('visible');
+                // setOpenLargeMedia(token);
+              }}
+            />
+          )}
+        </div>
+        {/* DETAILS */}
+        <div className='w-full flex flex-col gap-1 py-2 px-1'>
+          <div className='my-1'>{token.token.name}</div>
+          <div className='text-sm text-gray-400 hyphens-auto'>
+            {token.token.description}
+          </div>
         </div>
       </div>
-    </div>
+
+      {/* OPEN TOKEN MODAL */}
+      {openToken && (
+        <GridOpenToken onClose={() => setOpenToken(null)} token={token} />
+      )}
+    </>
   );
 }
