9b7223dd89d4413fa9d5776018a37ede2634603e
Vitor Butkus <vitorbutkus@gmail.com>
2025-05-19 20:18:10 -0300

    test contract Frags

--- src/utils/contract.js
+++ src/utils/contract.js
@@ -1,6 +1,6 @@
 // dummy Frags contract
-// const contract = '0x8e038a4805d984162028f5978acd894fad310b56';
+const contract = '0x8e038a4805d984162028f5978acd894fad310b56';
 
-const contract = '0xe5a192aaf911c35fb47de1342e768ef01c84fa09';
+// const contract = '0xe5a192aaf911c35fb47de1342e768ef01c84fa09';
 
 export default contract;
