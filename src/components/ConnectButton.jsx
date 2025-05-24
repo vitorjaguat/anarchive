8a5644b789bbcc543e7284ac54954ced9ebfe090
Vitor Butkus <vitorbutkus@gmail.com>
2025-05-24 15:54:38 -0300

    step 1 to improve performance: refactor getOrCreateSprite

--- src/components/ConnectButton.jsx
+++ src/components/ConnectButton.jsx
@@ -1,5 +1,6 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { truncate } from '@/utils/utils';
+import Image from 'next/image';
 
 export default function ConnectBtn() {
   return (
@@ -16,7 +17,7 @@ export default function ConnectBtn() {
         return (
           <button
             type='button'
-            className='px-4 py-2 rounded bg-sph-purple-light font-inter font-bold hover:bg-[#00cc00] text-white hover:text-black transition text-sm duration-300 ease-out'
+            className='w-full px-4 py-2 rounded bg-sph-purple-light font-inter font-bold hover:bg-[#00cc00] text-white hover:text-black transition text-sm duration-300 ease-out'
             onClick={
               !mounted
                 ? undefined
@@ -29,16 +30,23 @@ export default function ConnectBtn() {
           >
             <div
               className={
-                'translate-y-[1px] flex gap-4 items-center ' +
+                'w-full translate-y-[1px] flex flex-col gap-0  items-center ' +
                 (mounted && account ? 'text-sm' : '')
               }
             >
               {mounted && account ? (
                 <>
-                  <div className=''>{account.displayBalance}</div>
-                  <div className=''>
-                    {account.ensName || account.displayName}
+                  <div className='flex items-center'>
+                    {account.ensAvatar && (
+                      <Image
+                        src={account.ensAvatar}
+                        className='w-4 h-4 inline mr-2 '
+                      />
+                    )}
+                    {account.displayName}
                   </div>
+                  <div className='h-[1px] w-2/3 bg-black/50 rounded-sm'></div>
+                  <div className=''>{account.displayBalance}</div>
                 </>
               ) : (
                 'Connect'
