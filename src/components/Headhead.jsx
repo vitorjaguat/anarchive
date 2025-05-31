1a16099476194bb6917adfc07f94c5d552c02f75
Vitor Butkus <vitorbutkus@gmail.com>
2025-05-31 12:33:01 -0300

    add cannonical in HeadHead

--- src/components/Headhead.jsx
+++ src/components/Headhead.jsx
@@ -1,38 +1,41 @@
 import Head from 'next/head';
-// import favicon from '../../public/meta/image1.png';
-// import ogimage from '../../public/meta/image2.png';
 
-export default function Headhead({ ogImage, title, description }) {
+export default function Headhead({
+  ogImage,
+  title,
+  description,
+  canonicalUrl,
+}) {
+  const baseUrl = 'https://anarchiving.thesphere.as';
+  const fullCanonicalUrl = canonicalUrl ? `${baseUrl}${canonicalUrl}` : baseUrl;
+
   return (
     <>
       <Head>
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width' />
-        <link
-          rel='icon'
-          href='https://the-anarchive.vercel.app/meta/image1.png'
-        />
+
+        {/* Add canonical URL */}
+        <link rel='canonical' href={fullCanonicalUrl} />
+
+        <link rel='icon' href={`${baseUrl}/meta/image1.png`} />
         <title>{title}</title>
         <meta name='description' content={description} />
+
+        {/* Update all URLs to use custom domain */}
         <meta name='twitter:card' content='summary_large_image' />
         <meta name='twitter:site' content='@thesphere_as' />
         <meta name='twitter:title' content={title} />
         <meta name='twitter:description' content={description} />
-        <meta
-          name='twitter:image'
-          // content='https://the-anarchive.vercel.app/meta/image2.png'
-          content={ogImage}
-        />
+        <meta name='twitter:image' content={ogImage} />
+
         <meta property='og:type' content='website' />
+        <meta property='og:url' content={fullCanonicalUrl} />
         <meta property='og:determiner' content='the' />
         <meta property='og:locale' content='en' />
         <meta property='og:title' content={title} />
         <meta property='og:description' content={description} />
-        <meta
-          property='og:image'
-          // content='https://the-anarchive.vercel.app/meta/image2.png'
-          content={ogImage}
-        />
+        <meta property='og:image' content={ogImage} />
         <meta property='og:image:type' content='image/png' />
         <meta property='og:image:width' content='1200' />
         <meta property='og:image:height' content='630' />
