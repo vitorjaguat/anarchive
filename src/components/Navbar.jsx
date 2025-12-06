4fa1a81f2600e43fcb0163df2b0fcbc1fad9dfaf
Vitor Butkus <vitorbutkus@gmail.com>
2025-12-06 13:02:54 -0300

    minor adjustments; remove justify-between from TokenInfo right column; remove cursor-help from title at Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ export default function Navbar({
   return (
     <div className='w-full flex justify-between items-center p-4 select-none h-[100px] z-[1000] backdrop-blur-[6px] bg-slate-800/20'>
       <div
-        className='tracking-widest flex leading-4 z-[1000] hover:text-slate-400 duration-300 cursor-help items-center pr-20'
+        className='tracking-widest flex leading-4 z-[1000] hover:text-slate-400 duration-300 items-center pr-20'
         // onMouseEnter={() => setInfoVisible(true)}
         // onMouseLeave={() => setInfoVisible(false)}
       >
