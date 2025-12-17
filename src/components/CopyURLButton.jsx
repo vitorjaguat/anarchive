90cd05495de11e3c7beb6015474bf52887778efd
Vitor Butkus <vitorbutkus@gmail.com>
2025-12-17 13:43:09 -0300

    adjust open FULL for mobile in GridOpenToken and CopyURLButton

--- src/components/CopyURLButton.jsx
+++ src/components/CopyURLButton.jsx
@@ -12,13 +12,13 @@ export default function CopyURLButton() {
   return (
     <div
       className={
-        'mt-1 w-fit flex items-center gap-1 text-xs rounded-md px-2 py-[2px] bg-[#8989dc]/20 hover:bg-[#8989dc]/40 duration-300 cursor-grab ' +
+        'mt-1 w-fit flex items-center gap-1 text-xs rounded-md px-2 py-1 md:py-[2px] bg-white/20 md:bg-[#8989dc]/20 hover:bg-[#8989dc]/40 duration-300 cursor-grab ' +
         (copied && 'bg-[#00ff20]/10 hover:bg-[#00ff20]/20')
       }
       onClick={handleCopyLink}
     >
       <BsLink size='16' />
-      <div className='font-thin'>{!copied ? 'copy URL' : 'copied!'}</div>
+      <div className='md:font-thin'>{!copied ? 'copy URL' : 'copied!'}</div>
     </div>
   );
 }
