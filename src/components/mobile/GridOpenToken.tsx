8e9929234e3ae7d64df27e3b05893b36431bb4e0
Vitor Butkus <vitorbutkus@gmail.com>
2025-12-17 13:43:09 -0300

    adjust open FULL for mobile in GridOpenToken and CopyURLButton

--- src/components/mobile/GridOpenToken.tsx
+++ src/components/mobile/GridOpenToken.tsx
@@ -10,6 +10,8 @@ import LargeMedia from '../LargeMedia';
 import Markdown from 'react-markdown';
 // import Mint from '../Mint';
 import CollectModal from '../CollectModal';
+import { BsArrowsFullscreen } from 'react-icons/bs';
+import CopyURLButton from '../CopyURLButton';
 
 export default function GridOpenToken({
   token,
@@ -31,25 +33,22 @@ export default function GridOpenToken({
   return createPortal(
     <div className='fixed inset-0 z-50 flex items-center justify-center bg-black/70 backdrop-blur-[2px]'>
       <div
-        className='bg-slate-500 rounded-lg max-w-lg w-full max-h-[90vh] overflow-y-auto p-4'
+        className='relative bg-slate-500 rounded-lg max-w-lg w-full max-h-[90vh] overflow-y-auto p-4'
         tabIndex={-1}
       >
-        <button className='absolute top-2 right-2 blur-none' onClick={onClose}>
+        <button className='absolute top-3 right-3 blur-none' onClick={onClose}>
           <IoCloseOutline className='text-white' size={25} />
         </button>
         {/* IMAGE */}
-        <div className='w-full aspect-square flex items-center bg-black/20'>
+        <div className='mt-8 w-full aspect-square flex items-center bg-black/20'>
           {token?.token?.image && (
             <Image
               src={token.token.image}
               alt={token.token.name}
-              //   layout='responsive'
               width={400}
               height={400}
               className={'w-full cursor-pointer'}
-              //   onLoad={(e) => setImageLoaded(true)}
               onClick={() => {
-                // largeMediaControls.start('visible');
                 setOpenLargeMedia(token);
               }}
             />
@@ -61,27 +60,11 @@ export default function GridOpenToken({
             <GoPlusCircle size={18} color='#cbd5e1' />
             <div className='text-sm'>{token?.token?.totalMinted}</div>
           </div>
-          {/* <MintModal
-            chainId={7777777}
-            copyOverrides={{
-              mintTitle: 'Collect your own',
-              mintCtaBuy: 'Collect',
-            }}
-            trigger={
-              <button className='px-4 py-1 rounded-md bg-[#01ff00] text-[#000000] hover:scale-[1.02] transition-all duration-300'>
-                Collect
-              </button>
-            }
-            onMintComplete={(data) => console.log(data)}
-            onMintError={(error) => console.log(error)}
-            onConnectWallet={openConnectModal}
-            token={token.token.contract + ':' + token.token.tokenId}
-          /> */}
+
           <button
             className='px-3.5 text-sm py-1 tracking-wide rounded-md bg-[#01ff00] text-[#000000] hover:scale-[1.02] transition-all duration-300 translate-y-0.5 -translate-x-0.5'
             onClick={() => setOpenCollect(true)}
           >
-            {/* <Mint token={token.token} /> */}
             Collect
           </button>
         </div>
@@ -90,6 +73,22 @@ export default function GridOpenToken({
         <div className='flex flex-col gap-5 my-3 px-1'>
           {/* TITLE */}
           <div className=''>{token.token.name}</div>
+
+          {/* copy link + open FULL */}
+          <div className='-mt-1 flex justify-between gap-2'>
+            <div
+              className={
+                'mt-1 w-fit flex items-center gap-2 text-xs rounded-md px-2 py-1 md:py-[2px] bg-white/20 md:bg-[#8989dc]/20 hover:bg-[#8989dc]/40 duration-300 cursor-pointer '
+              }
+              onClick={() => {
+                setOpenLargeMedia(token);
+              }}
+            >
+              <BsArrowsFullscreen size='10' />
+              <div className='md:font-thin'>open FULL</div>
+            </div>
+            <CopyURLButton />
+          </div>
 
           {/* ATTRIBUTES */}
           <div className='grid grid-cols-2 gap-1'>
