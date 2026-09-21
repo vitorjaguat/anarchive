1743cf912006622ad70e6fb25b3f91911b8cffe3
Vitor Butkus <vitorbutkus@gmail.com>
2026-09-21 19:43:50 -0300

    minimal UI adjustments

--- src/components/InfoButton.tsx
+++ src/components/InfoButton.tsx
@@ -13,7 +13,7 @@ export default function InfoButton({
 }: InfoButtonProps) {
   return (
     <div
-      className='group grid grid-cols-[34px_0fr] hover:grid-cols-[34px_1fr] items-center cursor-help rounded-md bg-slate-500/40 h-[34px] overflow-hidden transition-[grid-template-columns,background-color] duration-500 ease-[cubic-bezier(0.4,0,0.2,1)]'
+      className='group grid grid-cols-[34px_0fr] hover:grid-cols-[34px_1fr] items-center cursor-help rounded-md bg-slate-500/40 hover:bg-slate-700/90 h-[34px] overflow-hidden transition-[grid-template-columns,background-color] duration-500 ease-[cubic-bezier(0.4,0,0.2,1)]'
       onClick={() => setInfoVisible(!infoVisible)}
     >
       <div className='flex items-center justify-center'>
